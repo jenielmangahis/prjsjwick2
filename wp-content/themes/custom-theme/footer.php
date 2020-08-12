362906e6166f58e5b66edf4d4b859db72c7b31a3
Jeniel Mangahis <jeniel.mangahis.work@gmail.com>
2020-08-12 15:51:55 +0800

    Updated footer and header

--- wp-content/themes/custom-theme/footer.php
+++ wp-content/themes/custom-theme/footer.php
@@ -70,22 +70,6 @@
           $(".nav-desktop").removeClass("cl-hide");
         }
     });
-
-    $('#carousel-a, #carousel-b').carousel({
-      pause: true,
-      interval: false
-    });
-
-    $('div.parallelogram-mobile-default').click(function() {
-      if ($('section.home-mobile-menu').hasClass('hide-mobile')) {
-        $('section.home-mobile-menu').removeClass('hide-mobile');
-        $('section.home-mobile-menu').addClass('show-mobile');
-      } else {
-        $('section.home-mobile-menu').addClass('hide-mobile');
-        $('section.home-mobile-menu').removeClass('show-mobile');
-      }
-    });
-    // add jquery code here
   });
 </script>
 <?php wp_footer();?>
--- wp-content/themes/custom-theme/header.php
+++ wp-content/themes/custom-theme/header.php
@@ -19,7 +19,8 @@
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/mobile_menu.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
-	<script defer type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.min.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
 	<link rel='stylesheet' id='main style-css'  href='<?php bloginfo('template_directory'); ?>/style.css?<?php echo date('l jS \of F Y h:i:s A'); ?>' media='all' />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -55,12 +56,10 @@
 	<div class="navigation-container">
 		<div class="col-5 left pl-0 pr-0 home-logo">
 			<div id="logo">
-				<a href="<?php echo home_url('/'); ?>">
-					<img src="https://sjwickcpa.com/wp-content/uploads/2018/07/long-logo.png" class="logo-default" />
-				</a>
+				<img src="http://localhost/tim/wordpress/prjsjwick2/wp-content/uploads/2018/07/long-logo.png" class="logo-default" />
 			</div>
 		</div>
-		<div class="col-7 left pl-0 pr-0 nav-desktop cl-collapse">
+		<div class="col-7 left pl-0 pr-0 nav-desktop">
 			<?php
 				$v = 0;
 				$menuargs = array(
@@ -71,59 +70,12 @@
 				$items = wp_get_nav_menu_items( 'MAIN-MENU', $menuargs);
 			?>
 			<nav class="clearfix">
-				<ul id="header-desktop-menu" class="header-desktop-menu">
-						<li class="menu-item"><a href="#">Home</a></li>
-						<li class="menu-item carit">
-							<a href="#">About us</a>
-
-							<ul class="sub-menu">
-								<li id="menu-item-323" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-323"><a>Services</a>
-									<ul class="sub-menu" style="">
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-316"><a href="https://sjwickcpa.com/services/accounting-bookkeeping/">Accounting &amp; Bookkeeping</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-317"><a href="https://sjwickcpa.com/services/business-consulting/">Business Consulting</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1694"><a href="https://sjwickcpa.com/services/cfo-services/">CFO Services</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-318"><a href="https://sjwickcpa.com/services/payroll-services/">Payroll Services</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-319"><a href="https://sjwickcpa.com/services/quickbooks-accounting/">QuickBooks Accounting</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-320"><a href="https://sjwickcpa.com/services/small-business-package/">Small Business Package</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-321"><a href="https://sjwickcpa.com/services/tax-service/">Tax Service</a></li>
-									</ul>
-								</li>
-								<li class="rhaenys menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1535"><a>Industries</a>
-									<ul class="sub-menu" style="">
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-303"><a href="https://sjwickcpa.com/areas-of-practice/construction/">Construction Accounting</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-307"><a href="https://sjwickcpa.com/areas-of-practice/manufacturing-accounting/">Manufacturing Accounting</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-309"><a href="https://sjwickcpa.com/areas-of-practice/restaurant-business/">Restaurant Business</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-310"><a href="https://sjwickcpa.com/areas-of-practice/retail-wholesale-accounting/">Retail &amp; Wholesale Accounting</a></li>
-									</ul>
-								</li>
-								<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1553"><a href="#">Our Office</a>
-									<ul class="sub-menu" style="">
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-903"><a href="https://sjwickcpa.com/our-office/">About Us</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-902"><a href="https://sjwickcpa.com/steve-wick/">Steve Wick</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-901"><a href="https://sjwickcpa.com/staff-members/">Staff Members</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-900"><a href="https://sjwickcpa.com/testimonials/">Testimonials</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1346"><a href="https://sjwickcpa.com/whats-important-to-us/">What’s Important to Us</a></li>
-									</ul>
-								</li>
-								<li class="rhaenys menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1536"><a href="#">Serving Clients In</a>
-									<ul class="sub-menu" style="">
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-306"><a href="https://sjwickcpa.com/areas-of-practice/loveland/">Loveland</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-304"><a href="https://sjwickcpa.com/areas-of-practice/fort-collins/">Fort Collins</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-322"><a href="https://sjwickcpa.com/areas-of-practice/windsor/">Windsor</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-305"><a href="https://sjwickcpa.com/areas-of-practice/greeley/">Greeley</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-312"><a href="https://sjwickcpa.com/areas-of-practice/wellington/">Wellington</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-311"><a href="https://sjwickcpa.com/areas-of-practice/timnath/">Timnath</a></li>
-										<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-302"><a href="https://sjwickcpa.com/areas-of-practice/berthoud/">Berthoud</a></li>
-									</ul>
-								</li>
-							</ul>
-
-						</li>
-						<li class="menu-item carit"><a href="#">Tools</a></li>
-						<li class="menu-item carit"><a href="#">Support</a></li>
-						<li class="menu-item"><a href="#">Contact</a></li>
+				<!-- <ul id="header-desktop-menu" class="header-desktop-menu">
+					<?php foreach( $items as $item ){ ?>
+						<li class="menu-item"><a href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a></li>
+					<?php } ?>
 						<li class="menu-item"><a href="#"><i class="fa fa-search search-nav" aria-hidden="true"></i></a></li>
-				</ul>
+				</ul> -->
 			</nav>
 		</div>
 	</div>
